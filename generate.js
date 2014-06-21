d2e88d2821fb6c453908181ebe0c63e4e1847ac8
Alexander Selzer <aselzer3@gmail.com>
2014-06-21 22:50:44 +0200

    Stuff

--- generate.js
+++ generate.js
@@ -1,1 +1,26 @@
 // Uhm...
+var fs = require("fs");
+var Handlebars = require("handlebars");
+
+// so, this isn't built in...
+Handlebars.registerHelper('times', function(n, block) {
+    var res = ''; // (html out)
+    for (var i = 0; i < n; ++i)
+        res += block.fn(i);
+    return res;
+});
+
+var weeks = require("./weeks.json");
+var results = [];
+
+var days = Handlebars.compile(fs.readFileSync("./days.hbs").toString());
+var notes = Handlebars.compile(fs.readFileSync("./notes.hbs").toString())
+
+weeks.forEach(function(week) {
+  results.push({
+    days: days(week),
+    notes: notes()
+  });
+});
+
+console.log(results);
